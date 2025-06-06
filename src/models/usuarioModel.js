50f23245dca0d0ef3109f0fe2a58d7bf97a7309d
DanL0pes <daniel.sousa@sptech.school>
2025-06-06 12:27:34 -0300

    Ajustando rota usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -21,7 +21,7 @@ function cadastrar(nome, senha) {
 }
 
 function retornar(idUsuario){
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ")
     var instrucaoSql = `
     SELECT id, nome FROM usuario
     WHERE id = ${idUsuario};
