e7ed5c959491ba740254eec8c6c1eaadd3f7e18d
DanL0pes <daniel.sousa@sptech.school>
2025-06-06 12:31:31 -0300

    Tirando email da model usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 function autenticar(nome, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", senha)
     var instrucaoSql = `
     SELECT id, nome, senha FROM usuario
     WHERE nome = '${nome}' AND senha = '${senha}';
